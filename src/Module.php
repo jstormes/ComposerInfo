53f524a8bfb2f3c6bbedd67f36f1aff71f9ea54f
James Stormes <james.s@digitalroominc.com>
2017-08-04 11:10:59 -0500

    Helps if you put the file in the correct directory!!!

--- src/Module.php
+++ src/Module.php
@@ -8,25 +8,6 @@ use Zend\Db\TableGateway\TableGateway;
 use Zend\Stdlib\Hydrator\ArraySerializable;
 use Zend\Db\TableGateway\Feature\MasterSlaveFeature;
 
-use Auth\Service\AuthService;
-use User\Model\UsersTable;
-use Job\Model\JobItemsTable;
-use DataAccess\DataAccessResultSet;
-//use DataAccess\IMS\Table\ManfImpositionXmlSet;
-
-use Imposition\Model\ImpositionSumary;
-use Imposition\Model\JobTableFilter;
-use Imposition\Model\JobTableFilterTable;
-use Imposition\Model\ImpositionJobItems;
-use Imposition\Model\ImpositionJobItemsSelected;
-use Imposition\Table\JobsWorkingDetailView;
-
-use Imposition\Model\ImpositionXmlSets;
-use Imposition\Model\ImpositionXmlSetsTable;
-use Imposition\Service\JobItemService;
-use Imposition\Service\AwsS3ImpositionService;
-
-//use Imposition\Exception\ImpositionException;
 
 class Module
 {
